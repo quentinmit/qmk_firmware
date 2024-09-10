68613a8f36ea90bde390b3246219bf6890651eee
Olly Smith <olly.smith@gmail.com>
2024-09-10 18:54:36 +0100

    Make volume key behaviour match stock firmware.

--- keyboards/clockworkpi/uconsole/keymaps/default/keymap.c
+++ keyboards/clockworkpi/uconsole/keymaps/default/keymap.c
@@ -16,6 +16,11 @@ enum {
   JS_DOWN
 };
 
+const key_override_t vol_key_override =
+  ko_make_basic(MOD_MASK_SHIFT, KC_VOLD, KC_VOLU);
+
+const key_override_t *key_overrides[] = {&vol_key_override};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -27,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, KC_RSFT, KC_LCTL, KC_RCTL, KC_LALT, MS_BTN1, KC_RALT, MS_BTN2,
         MS_BTN3, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 
-        JS_4,    JS_5,    KC_VOLU, KC_GRV,  KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,
+        JS_4,    JS_5,    KC_VOLD, KC_GRV,  KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,
         KC_9,    KC_0,    KC_ESC,  KC_TAB,  KC_NO,   KC_NO,   KC_NO,   KC_NO,
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,
@@ -42,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, KC_LGUI, _______, KC_RGUI, _______,
         _______, _______, _______, _______, _______, _______, _______, _______,
 
-        KC_PSCR, KC_PAUS, KC_VOLD, _______, _______, _______, KC_F11,  KC_F12,
+        KC_PSCR, KC_PAUS, KC_MUTE, _______, _______, _______, KC_F11,  KC_F12,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
         KC_F9,   KC_F10,  QK_BOOT, KC_CAPS, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, KC_PGUP, KC_INS,
